709a1fc1b4533e84529f1b05baa85501d8313487
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-11-30 11:56:54 +0900

    fix: create index in the create table statement

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -8,7 +8,9 @@ CREATE TABLE competition (
   title TEXT NOT NULL,
   finished_at BIGINT NULL,
   created_at BIGINT NOT NULL,
-  updated_at BIGINT NOT NULL
+  updated_at BIGINT NOT NULL,
+  INDEX idx_tenant_id (tenant_id),
+  INDEX idx_tenant_id_created_at (tenant_id, created_at DESC),
 );
 
 CREATE TABLE player (
@@ -17,7 +19,8 @@ CREATE TABLE player (
   display_name TEXT NOT NULL,
   is_disqualified BOOLEAN NOT NULL,
   created_at BIGINT NOT NULL,
-  updated_at BIGINT NOT NULL
+  updated_at BIGINT NOT NULL,
+  INDEX idx_tenant_id_created_at (tenant_id, created_at DESC),
 );
 
 CREATE TABLE player_score (
@@ -28,11 +31,7 @@ CREATE TABLE player_score (
   score BIGINT NOT NULL,
   row_num BIGINT NOT NULL,
   created_at BIGINT NOT NULL,
-  updated_at BIGINT NOT NULL
+  updated_at BIGINT NOT NULL,
+  INDEX idx_tenant_id_competition_id (tenant_id, competition_id),
+  INDEX idx_tenant_id_competition_id_player_id_row_num (tenant_id, competition_id, player_id, row_num),
 );
-
-CREATE INDEX idx_competition_tenant_id ON competition (tenant_id);
-CREATE INDEX idx_player_tenant_id_created_at ON player (tenant_id, created_at DESC);
-CREATE INDEX idx_competition_tenant_id_created_at ON competition (tenant_id, created_at DESC);
-CREATE INDEX idx_player_score_tenant_id_competition_id_player_id_row_num ON player_score (tenant_id, competition_id, player_id, row_num);
-CREATE INDEX idx_player_score_tenant_id_competition_id ON player_score (tenant_id, competition_id);
