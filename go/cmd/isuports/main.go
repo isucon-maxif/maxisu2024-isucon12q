baafdd664c6d04296ee5d1da95fba7b50ba26fb2
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-11-30 11:19:32 +0900

    chore: add pprof

--- go/cmd/isuports/main.go
+++ go/cmd/isuports/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
+
 	isuports "github.com/isucon/isucon12-qualify/webapp/go"
 )
 
 func main() {
+	runtime.SetBlockProfileRate(1)
+	runtime.SetMutexProfileFraction(1)
+	go func() {
+		log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
+	}()
 	isuports.Run()
 }
