89d4f2d12979235c971544469f609c5521d79376
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-11-30 16:31:24 +0900

    chore: split s2 for admin db

--- sql/init.sh
+++ sql/init.sh
@@ -12,7 +12,7 @@ ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 # MySQLを初期化
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
+		--host "192.168.0.4" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
