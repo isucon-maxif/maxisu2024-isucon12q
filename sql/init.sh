f1a6ebda52d44b874cccf77aee7d6de5795e19c1
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-11-30 14:46:17 +0900

    feat: init 処理更新

--- sql/init.sh
+++ sql/init.sh
@@ -9,13 +9,28 @@ ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
-# MySQLを初期化
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
+# MySQLを初期化 (s2)
+mysql -u"isucon" \
+		-p"isucon" \
+		--host "192.168.0.4" \
 		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < init.sql
+		"isuports" < init.sql
 
 # SQLiteのデータベースを初期化
 rm -f ../tenant_db/*.db
 cp -r ../../initial_data/*.db ../tenant_db/
+
+# SQLite のデータを MySQL にインポート (s3)
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		-e "DROP DATABASE IF EXISTS isuports_tenants; CREATE DATABASE isuports_tenants;"
+for db in ../tenant_db/*.db; do
+	./sqlite3_to_sql $db > ./import.sql
+	mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"isuports_tenants" < ./import.sql
+done
